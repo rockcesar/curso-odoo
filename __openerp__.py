014f93d9bc4b9a2d8afb9e45a1c04ef6c271dd70
César Cordero <cesar.cordero.r@gmail.com>
2014-09-16 11:05:10 -0430

    Creación de los dos archivos iniciales del repositorio.

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+    En cabecera:
+    #!/usr/bin/env python
+'''
+
+{
+    'name':'Curso OpenERP',
+    'description':'Módulo es para aprender OpenERP 7.0',
+    'author': 'César Cordero',
+    'version':'dia1',
+    'depends':['base', ],
+    'data':[],
+    'demo':[],
+}
